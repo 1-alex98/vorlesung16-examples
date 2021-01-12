1f4e3b1f333a4d2467ccf889a1fb3e877e9cb150
axel12 <alexander.von.trostorff@gmail.com>
2021-01-12 18:09:21 +0100

    add check for annotation

--- SampleEventBus/src/EventBus.java
+++ SampleEventBus/src/EventBus.java
@@ -1,6 +1,8 @@
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class EventBus {
@@ -15,6 +17,12 @@ public class EventBus {
         for (Object listener: registered){
             Method[] methods = listener.getClass().getMethods();
             for(Method method: methods){
+                Annotation[] declaredAnnotations = method.getDeclaredAnnotations();
+                boolean hasSubscribeAnnotation = Arrays.stream(declaredAnnotations)
+                        .anyMatch(annotation -> annotation.annotationType() == Subscribe.class);
+                if(!hasSubscribeAnnotation){
+                    continue;
+                }
                 Class<?>[] parameterTypes = method.getParameterTypes();
 
                 if(parameterTypes.length == 1 && parameterTypes[0] == event.getClass()){
